50125c2482a7cc64c76e667f64b3867859e4e347
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-03-25 10:38:02 -0400

    First cut at postinst configuration.

---
+++ make_package.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+fpm -s dir -t deb \
+    --name "grizzly-network" \
+    --prefix "/usr/share/grizzly-network" \
+    --version 0.1.0 \
+    --architecture all \
+    --after-install debian/postinst \
+    --deb-templates debian/templates \
+    --deb-config debian/config \
+    --depends dnsmasq
+    --depends hostapd
+    ./templates
